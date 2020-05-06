556a177310c2bbdc5f21f2d9632d5158378630b9
kaelaclement <stephanie.kaela@gmail.com>
2020-05-06 12:33:23 +0100

    get images to display

--- challenges/dynamicHtml/index.js
+++ challenges/dynamicHtml/index.js
@@ -1,22 +1,31 @@
+// 
+
 const searchButton = document.getElementById('searchButton');
+const clearButton = document.getElementById('clearButton');
 const searchInput = document.getElementById('searchTerm');
 const numResults = document.getElementById('number');
 const wrapper = document.getElementById('wrapper');
 
-const makeDivs = () => {
-	const term = searchInput.value;
-	const num = numResults.value;
-
+const makeDivs = (term, num) => {
 	for (let i = 0; i < num; i++) {
-		let div = document.createElement('div');
-		let content = document.createTextNode(term);
-		div.className = 'box';
-		div.appendChild(content);
-		wrapper.appendChild(div);
+		let img = document.createElement('img');
+		img.src = `https://source.unsplash.com/random/500x500/?sig=${i}&${term}`;
+		img.className = 'box';
+		wrapper.appendChild(img);
 	}
 }
 
 
 searchButton.addEventListener('click', () => {
-	makeDivs();
+	if (isNaN(numResults.value)) {
+		alert('Please enter a number');
+	} else {
+		const term = searchInput.value;
+		const num = parseInt(numResults.value);
+		makeDivs(term, num);
+	}
+})
+
+clearButton.addEventListener('click', () => {
+	wrapper.innerHTML = '';
 })
