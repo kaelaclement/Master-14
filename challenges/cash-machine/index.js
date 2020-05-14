1028585fad56407456a5f4051eb9158e12ec4bcf
kaelaclement <stephanie.kaela@gmail.com>
2020-05-14 12:18:49 +0100

    bring cash machine onto the dom

--- challenges/cash-machine/index.js
+++ challenges/cash-machine/index.js
@@ -1,25 +1,98 @@
 const wrapper = document.getElementById('wrapper');
+const transactions = document.getElementById('transactions');
 const pinButton = document.getElementById('pinSubmit');
 const pinEntered = document.getElementById('pin');
-const pin = 1234;
+const acctEntered = document.getElementById('account');
+const menu = document.getElementById('menu');
+const checkBalanceButton = document.getElementById('checkBalance');
+const withdrawButton = document.getElementById('withdrawal');
+const depositButton = document.getElementById('deposit');
 
-const checkPin = (userInput) => {
-	if (userInput == pin) {
-		wrapper.innerHTML = menu();
-	} else {
-		alert('what happened');
+let currentUser;
+
+let accounts = {
+	123456789: {
+		pin: 1234,
+		balance: 1000
 	}
 }
 
-pinButton.addEventListener('click', () => {
-	checkPin(pinEntered.value);
-	pinEntered.value = '';
-});
+const checkAccount = (accountNumber) => {
+	let account = accounts[accountNumber];
+	if (account) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
-const menu = () => {
-	const menuText =
-		`<p>What would you like to do?</p><br>
-	<input type="button" id="checkBalance"><br>
-	<input type="button" id="withdrawal">`
-	return menuText;
+const checkPin = (accountNumber, pin) => {
+	let account = accounts[accountNumber];
+	if (checkAccount(accountNumber) && account.pin == pin) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+const checkBalance = () => {
+	let content = document.createElement('p');
+	content.innerText = `Your balance is: £${currentUser.balance}`;
+	transactions.appendChild(content);
+}
+
+const withdraw = () => {
+	let amount = parseInt(prompt('How much would you like to withdraw?'));
+	currentUser.balance = currentUser.balance - amount;
+	let content = document.createElement('p')
+	content.innerText = `Withdrawal: £${amount}\nNew Balance: £${currentUser.balance}`
+	transactions.appendChild(content);
+}
+
+const showMenu = () => {
+	menu.style.display = 'flex';
+}
+
+const hideMenu = () => {
+	menu.style.display = 'none';
+}
+
+// pinButton.addEventListener('click', () => {
+// 	let tryAccount = acctEntered.value;
+// 	let tryPin = pinEntered.value;
+
+// 	if (checkPin(tryAccount, tryPin)) {
+// 		showMenu();
+// 	}
+// 	pinEntered.value = '';
+// 	acctEntered.value = '';
+// });
+
+pinButton.onclick = () => {
+	let tryAccount = acctEntered.value;
+	let tryPin = pinEntered.value;
+
+	let accountExists = checkAccount(tryAccount);
+	let pinCorrect = checkPin(tryAccount, tryPin);
+
+	if (accountExists && pinCorrect) {
+		currentUser = accounts[tryAccount];
+		showMenu();
+	} else if (accountExists) {
+		alert('Please enter correct PIN');
+	} else {
+		alert('Cannot find account');
+	}
+	pinEntered.value = '';
+	acctEntered.value = '';
+}
+
+checkBalanceButton.onclick = checkBalance;
+withdrawButton.onclick = withdraw;
+depositButton.onclick = () => {
+	let amount = parseInt(prompt('How much would you like to deposit?'));
+	currentUser.balance = currentUser.balance + amount;
+	let content = document.createElement('p');
+	content.innerText = `Deposit: £${amount}\nNew Balance: £${currentUser.balance}`
+	transactions.appendChild(content);
 }
